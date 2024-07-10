f2b600390d948b1120a56e20361bdd9adf6fe015
Leann Biancani <biancani@ssh3.hac.uri.edu>
2024-07-10 16:37:58 -0400

    update iqtree scripts and gitignore

--- 01_iqtree/00_iqtree_prep.sh
+++ 01_iqtree/00_iqtree_prep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name="IQprep"
-#SBATCH --time=1:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --time=0:30:00  # walltime limit (HH:MM:SS)
 #SBATCH --mail-user="biancani@uri.edu" #CHANGE THIS to your user email address
 #SBATCH --mail-type=ALL
 #SBATCH --nodes=1   # number of nodes
@@ -18,19 +18,20 @@ scripts_dir=$PROJECT/01_iqtree
 # path to aligned loci:
 aligned_loci_path=$DATA/Fong/simulated_loci
 # path to output folder (will be created if doesn't exist):
-OUTPUT=$PROJECT/output/Fong/
+OUTPUT=$PROJECT/output/Fong
 # name of iqtree array work folder (will be created if doesn't exist):
 array_work_folder=iqtree_assessment
+# number of loci per array task 
+LociPerTask=50
 
 mkdir -p $OUTPUT
 cd $OUTPUT
 mkdir -p ${array_work_folder}
 cd ${array_work_folder}
-mkdir scf
-ls ${aligned_loci_path} | rev | cut -f1 -d/ | rev | split - aligned_loci_list_
+ls ${aligned_loci_path} | rev | cut -f1 -d/ | rev | split -l $LociPerTask - aligned_loci_list_
 arrayN=$(ls aligned_loci_list_* | wc -l)
 ls aligned_loci_list_* > array_list.txt
-ARRAY="sbatch --array=1-${arrayN}%40 ${scripts_dir}iqtree_array.sh"
-echo $ARRAY 
-echo $ARRAY >> array_command.txt
+ARRAY="#SBATCH --array=[1-${arrayN}]%40"
+echo $ARRAY
+echo $ARRAY >> sbatch_array_directive.txt
 
