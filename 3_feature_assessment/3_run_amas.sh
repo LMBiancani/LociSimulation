a889010a65c7e6a2a1e196dc235e1a5283a092a6
Molly Donnellan <molly_donnellan_uri_edu@login1.unity.rc.umass.edu>
2025-08-07 16:03:21 +0000

    adjusted SBATCH flags, adjusted module versions and path to AMAS, made an array, deleted for loop

--- 3_feature_assessment/3_run_amas.sh
+++ 3_feature_assessment/3_run_amas.sh
@@ -3,23 +3,31 @@
 #SBATCH --time=1:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --ntasks-per-node=12   # processor core(s) per node
+#SBATCH -p uri-cpu
+#SBATCH --mail-user="molly.donnellan@uri.edu" #CHANGE TO user email address
+#SBATCH --mail-type=ALL
+#SBATCH --array=[0-48]%49
 
 module purge
+module load uri/main
 module load Python/3.7.4-GCCcore-8.3.0
-amas="/home/aknyshov/alex_data/andromeda_tools/AMAS/amas/AMAS.py"
+amas="../../../../../AMAS/amas/AMAS.py"
 cores=12
 
 pwd
 date
 
-for i in ../simulations/*/*/1
-	do
+SPP=(../simulations/*/*/1)
+i=${SPP[$SLURM_ARRAY_TASK_ID]}
+
+#for i in ../simulations/*/*/1
+#	do
 	cd ${i}
 	pwd
 	ls
 	python ${amas} summary -c ${cores} -o amas_output2.txt -f fasta -d dna -i alignments2/*.fas 
 	python ${amas} summary -c ${cores} -o amas_output3.txt -f fasta -d dna -i alignments3/*.fas
 	cd ../../../../3_feature_assessment/
-done
+#done
 
 date
