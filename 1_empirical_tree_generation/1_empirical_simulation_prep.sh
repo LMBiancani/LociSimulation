f5fb67593900afeddec28776b583ac89d736a3e1
Molly Donnellan <molly_donnellan_uri_edu@login2.unity.rc.umass.edu>
2025-08-07 14:23:53 +0000

    added emails, uri cpu, commented out McGowen

--- 1_empirical_tree_generation/1_empirical_simulation_prep.sh
+++ 1_empirical_tree_generation/1_empirical_simulation_prep.sh
@@ -3,6 +3,9 @@
 #SBATCH --time=72:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --ntasks-per-node=1   # processor core(s) per node
+#SBATCH --mail-user="molly.donnellan@uri.edu" #CHANGE TO user email address
+#SBATCH --mail-type=ALL
+#SBATCH -p uri-cpu
 #SBATCH -c 2
 #SBATCH --mem-per-cpu=6G
 
@@ -13,15 +16,15 @@ cd ..
 pwd
 date
 
-#mkdir -p simulations/empirical/fong/1/
-#mkdir -p simulations/empirical/wickett/1/
-mkdir -p simulations/empirical/mcgowen/1/
+mkdir -p simulations/empirical/fong/1/
+mkdir -p simulations/empirical/wickett/1/
+#mkdir -p simulations/empirical/mcgowen/1/
 #mkdir -p simulations/empirical/liu/1/
 
-#cp datasets/Fong_alignments/*treefile simulations/empirical/fong/1/
+cp datasets/Fong_alignments/*treefile simulations/empirical/fong/1/
 
-#cp datasets/Wickett_alignments/*treefile simulations/empirical/wickett/1/
+cp datasets/Wickett_alignments/*treefile simulations/empirical/wickett/1/
 
-cp datasets/McGowen_alignments/*treefile simulations/empirical/mcgowen/1/
+#cp datasets/McGowen_alignments/*treefile simulations/empirical/mcgowen/1/
 
 #cp datasets/Liu_alignments/*treefile simulations/empirical/liu/1/
