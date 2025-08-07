f0e26f51c1e8d45f535c9ac828316e551db7db3a
Molly Donnellan <molly_donnellan_uri_edu@login1.unity.rc.umass.edu>
2025-08-07 15:46:32 +0000

    added email, uri cpu, made an array job, adjusted module versions, deleted for loop, made a variable path to INDELible, adjusted directory name

--- 2_simulation_scripts/5_INDELible.sh
+++ 2_simulation_scripts/5_INDELible.sh
@@ -3,8 +3,12 @@
 #SBATCH --time=72:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --ntasks-per-node=1   # processor core(s) per node
+#SBATCH -p uri-cpu
+#SBATCH --mail-user="molly.donnellan@uri.edu" #CHANGE TO user email address
+#SBATCH --mail-type=ALL
 #SBATCH -c 2
 #SBATCH --mem-per-cpu=6G
+#SBATCH --array=[0-48]%48
 
 ##This script prepares directories and control files for INDELible, runs INDELible for all four empirical datasets, and modifies INDELible alignments to introduce crosscontamination and missing data 
 
@@ -14,21 +18,26 @@
 pwd
 date
 
-module load R-bundle-Bioconductor/3.16-foss-2022b-R-4.2.2
+module load uri/main
+module load R-bundle-Bioconductor/3.15-foss-2021b-R-4.2.0
 
-for i in ../simulations/*/*/1/
-	do
+SPP=(../simulations/*/*/1/)
+i=${SPP[$SLURM_ARRAY_TASK_ID]}
+INDEL=(../../../../../../INDELibleV1.03/src/indelible)
+
+#for i in ../simulations/*/*/1/
+#	do
 	cd $i
 	pwd
-	Rscript ../../../../simulation_scripts/prep_INDELible.R gene_trees.tre df.csv
+	Rscript ../../../../2_simulation_scripts/prep_INDELible.R gene_trees.tre df.csv
 	cd alignments1
 	cp ../control.txt .
-	/data/schwartzlab/awalling/tools/INDELibleV1.03/src/indelible
+	$INDEL
 	cp ../controlCDS.txt ./control.txt
-	/data/schwartzlab/awalling/tools/INDELibleV1.03/src/indelible
+        $INDEL
 	cd ../
-	Rscript ../../../../simulation_scripts/post_INDELible.R alignments1 df.csv
-	cd ../../../../simulation_scripts/
+	Rscript ../../../../2_simulation_scripts/post_INDELible.R alignments1 df.csv
+	cd ../../../../2_simulation_scripts/
 	pwd
-done
+#done
 
