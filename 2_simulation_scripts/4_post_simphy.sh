6220e4e68c1bbbf140dbbcba83bec94af145e1f9
Molly Donnellan <molly_donnellan_uri_edu@login1.unity.rc.umass.edu>
2025-08-07 15:42:33 +0000

    changed wallclock, added email, uri cpu, deleted for loop

--- 2_simulation_scripts/4_post_simphy.sh
+++ 2_simulation_scripts/4_post_simphy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name="post_simphy"
-#SBATCH --time=72:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --time=96:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --ntasks-per-node=1   # processor core(s) per node
+#SBATCH -p uri-cpu
+#SBATCH --mail-user="molly.donnellan@uri.edu" #CHANGE TO user email address
+#SBATCH --mail-type=ALL
 #SBATCH -c 2
 #SBATCH --mem-per-cpu=6G
 
@@ -12,8 +15,8 @@ pwd
 date
 
 
-for i in ../simulations/*/*; do 
-    echo $i; 
+#for i in ../simulations/*/*; do 
+ #   echo $i; 
     while [[ $(ls ${i}/1/loc_*/1/g_trees1.trees | wc -l) -lt 2000 ]]; do
         for j in $(seq 1 2000); do # go through all loc_ (2000)
             if [ -s ${i}/1/loc_${j}/1/g_trees1.trees ]
