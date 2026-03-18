653d2769f1b022f0f4a648bab4212024c3c46c6a
Leann M. Biancani <biancani@uri.edu>
2026-03-18 23:12:21 +0000

    feat: refactor 3.0 for multi-simulation (via seeds) automation and centralized variables Multi-Seed Support: Added a for loop and positional argument  to iterate through all simulation sets. Variable Integration: Migrated hardcoded project paths, script locations, and input/output directories to variables.sh. Dynamic Pathing: Updated Alignments and out_dir to handle nested set_ directory structures.

---
+++ Scripts/3_filter_by_known_clades/3.0_submit_generate_constraints.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=submit_jobs
+#SBATCH --ntasks=1
+#SBATCH --mem=100M
+#SBATCH -p uri-cpu
+#SBATCH --time=00:05:00
+#SBATCH --mail-user="biancani@uri.edu"
+#SBATCH --mail-type=ALL
+
+# Source master parameters script:
+vars="/scratch4/workspace/biancani_uri_edu-LociSimulation/LociSimulation/Scripts/variables.sh"
+source $vars
+echo "Variables sourced into current shell environment:"
+cat $vars
+
+for random_seed in $seeds;
+  do
+  echo "Submitting generate constraints job for random seed: $random_seed";
+  sbatch $GenCon $random_seed
+done
--- Scripts/3_filter_by_known_clades/generate_constraints.R
+++ Scripts/3_filter_by_known_clades/generate_constraints.R
@@ -19,16 +19,16 @@ for (f in loci_files) {
   locus_path <- file.path(ALN_DIR, f)
   fasta <- read.FASTA(locus_path)
   present_taxa <- names(fasta)
-  
+
   # Intersect defined clades with what is actually in this locus
   pruned_clades <- lapply(all_clades, function(x) intersect(x, present_taxa))
   valid_clades <- pruned_clades[sapply(pruned_clades, length) > 1]
-  
+
   if (length(valid_clades) > 0) {
     # Create the constraint string
     clade_strings <- sapply(valid_clades, function(x) paste0("(", paste(x, collapse = ","), ")"))
     final_constraint <- paste0("(", paste(clade_strings, collapse = ","), ");")
-    
+
     # Save as locus_i_constraint.newick
     out_name <- gsub("\\.fas$", "_constraint.newick", f)
     write(final_constraint, file = file.path(OUTPUT_DIR, out_name))
